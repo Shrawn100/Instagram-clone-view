b8df1d75f5922b2432d39b4fc45951fd1e4d5b56
Shrawn <shrawn2002@gmail.com>
2023-08-21 21:23:43 +1000

    Setup signup page and functionality

--- src/Router.js
+++ src/Router.js
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import LoginForm from "./Login";
+import SignupForm from "./Signup";
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginForm />} />
+        <Route path="/signup" element={<SignupForm />} />
       </Routes>
     </BrowserRouter>
   );
--- src/Signup.js
+++ src/Signup.js
@@ -2,10 +2,11 @@ import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-const LoginForm = () => {
+const SignupForm = () => {
   const navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
   const [errors, setErrors] = useState([]);
   const [message, setMessage] = useState("");
 
@@ -15,49 +16,54 @@ const LoginForm = () => {
       setUsername(value);
     } else if (name === "password") {
       setPassword(value);
+    } else if (name === "displayName") {
+      setDisplayName(value);
     }
-  };
-  const handleLogin = async () => {
-    // Handle login with test user
-    setUsername("testUser");
-    setPassword("testUser");
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     try {
-      const response = await axios.post("http://localhost:3000/login", {
+      const response = await axios.post("http://localhost:3000/signup", {
+        name: displayName,
         username: username,
         password: password,
       });
-      const { message, errors, token } = response.data;
+      console.log(response.data);
+      const { message, errors } = response.data;
 
       if (errors) {
         setErrors(errors);
         setMessage("");
-      } else if (message === "User does not exist") {
+      } else if (message === "Validation failed") {
         setErrors([]);
-        setMessage("User does not exist");
-      } else if (message === "Wrong password") {
+        setMessage("Please correct the validation errors.");
+      } else if (message === "User registered successfully") {
         setErrors([]);
-        setMessage("Wrong password");
-      } else if (token) {
-        // Perform any necessary action upon successful login, e.g., save token to localStorage
-        localStorage.setItem("token", token);
-        setErrors([]);
-        setMessage("Login successful");
-        navigate("/home"); // Redirect to "/home" page
+        setMessage("Signup successful");
+        // You might want to perform any necessary action upon successful signup
+        navigate("/"); // Redirect to "/login" page
       }
     } catch (error) {
-      console.error("Login request failed:", error);
+      console.error("Signup request failed:", error);
     }
   };
 
   return (
-    <div className="login-page-container">
-      <h1 className="login-header">Welcome back</h1>
-      <form className="login-form" onSubmit={handleSubmit}>
+    <div className="signup-page-container">
+      <h1 className="signup-header">Create an account</h1>
+      <form className="signup-form" onSubmit={handleSubmit}>
+        <div className="input-container">
+          <label htmlFor="displayName">Display name</label>
+          <input
+            type="text"
+            id="displayName"
+            name="displayName"
+            value={displayName}
+            onChange={handleInputChange}
+          />
+        </div>
         <div className="input-container">
           <label htmlFor="username">Username</label>
           <input
@@ -79,27 +85,21 @@ const LoginForm = () => {
             onChange={handleInputChange}
           />
         </div>
-        <div className="login-page-btns">
-          <button type="submit">Submit</button>
-          <button type="button" onClick={handleLogin}>
-            Test User
-          </button>
+        <div className="signup-page-btns">
+          <button type="submit">Sign Up</button>
         </div>
 
         {errors.length > 0 && (
           <ul className="errors-list-ul">
             {errors.map((error, index) => (
-              <li key={index}>{error.msg}</li>
+              <li key={index}>{error}</li>
             ))}
           </ul>
         )}
         {message && <p className="error-msg-p">{message}</p>}
       </form>
-      <button onClick={navigate("/signup")}>
-        Don't have an account? Signup
-      </button>
     </div>
   );
 };
 
-export default LoginForm;
+export default SignupForm;
