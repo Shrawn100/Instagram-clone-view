dc277732e626f41879eae87e5f56fcc05d432ecb
Shrawn <shrawn2002@gmail.com>
2023-08-25 22:06:01 +1000

    Create fetching of  posts for homepage

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,31 @@
 import Navbar from "./Navbar";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function Home() {
+  const [postsList, setPostsList] = useState([]);
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      axios
+        .get("http://localhost:3000/home", config)
+        .then((response) => {
+          console.log(response.data.sortedPostsList);
+          setPostsList(response.data.sortedPostsList);
+        })
+        .catch((error) => {
+          console.error("Error fetching :", error);
+        });
+    } else {
+      //Unathorised
+    }
+  }, []);
   return (
     <>
       <Navbar />
