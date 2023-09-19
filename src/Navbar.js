f2130a86c8c63d1f0d1d06e45069a184ff4a0079
Shrawn <shrawn2002@gmail.com>
2023-09-19 12:16:51 +1000

    Fix modal closing when selecting file

--- src/Navbar.js
+++ src/Navbar.js
@@ -12,6 +12,9 @@ function Navbar() {
   function handleCloseClick(e) {
     if (dialogRef.current) {
       const dialogDimensions = dialogRef.current.getBoundingClientRect();
+      console.log(dialogDimensions);
+      console.log(e.clientX, e.clientY);
+
       if (
         e.clientX < dialogDimensions.left ||
         e.clientX > dialogDimensions.right ||
@@ -19,6 +22,14 @@ function Navbar() {
         e.clientY > dialogDimensions.bottom
       ) {
         dialogRef.current.close();
+        if (
+          dialogDimensions.left === 0 ||
+          dialogDimensions.right === 0 ||
+          dialogDimensions.top === 0 ||
+          dialogDimensions.bottom === 0
+        ) {
+          dialogRef.current.showModal();
+        }
       }
     }
   }
